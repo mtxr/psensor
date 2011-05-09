7490f73d95ba00b7318c9e631bcc6f5b8109292a
Jean-Philippe Orsini <jeanfi@gmail.com>
2011-05-09 07:32:11 +0000

    removed trailing space

--- src/lib/psensor.c
+++ src/lib/psensor.c
@@ -437,6 +437,6 @@ void psensor_list_update_measures(struct psensor **sensors)
 {
 	lmsensor_psensor_list_update(sensors);
 
-	if (psensor_list_contains_type(sensors, SENSOR_TYPE_HDD_TEMP)) 
+	if (psensor_list_contains_type(sensors, SENSOR_TYPE_HDD_TEMP))
 		hdd_psensor_list_update(sensors);
 }
