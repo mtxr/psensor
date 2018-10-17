6be042a0b71dae80c19f57a9668cc4711cab3b8a
Jean-Philippe Orsini <orsinije@fr.ibm.com>
2018-10-17 13:07:54 +0200

    Fixed the display of sensors of type 'percent'. Closed #4.

--- www/psensor.js
+++ www/psensor.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010-2011 jeanfi@gmail.com
+ * Copyright (C) 2010-2018 jeanfi@gmail.com
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -36,7 +36,7 @@ function format_mem_size(s) {
 
     if (k >= 1)
         return k+"Ko";
-    
+
     if (o > 0)
         return o+"o";
 
@@ -46,13 +46,13 @@ function format_mem_size(s) {
 function type_to_str(stype) {
     var stype_str;
 
-    if (stype & 0x0200) 
+    if (stype & 0x0200)
         stype_str = "NVidia ";
     else if (stype & 0x0800)
         stype_str = "ATI/AMD ";
-    else 
+    else
 	stype_str = "";
- 
+
     if (stype & 0x04000)
 	stype_str += "HDD ";
     else if (stype & 0x08000)
@@ -74,11 +74,13 @@ function type_to_str(stype) {
 
 function type_to_unit(stype) {
     if (stype & 0x0001)
-        unit = "C";
+        return "C";
     else if (stype & 0x0002)
-        unit = "RPM";
+        return "RPM";
+    else if (stype & 0x0004)
+        return "%";
 
-    return unit;
+    return "";
 }
 
 function value_to_str(value, type) {
@@ -109,7 +111,7 @@ function update_chart(chart_id, title, data) {
 
     measures = data["measures"];
     data_chart = [];
-    
+
     $("h1").html("");
     $("h1").append(data["name"]);
 
@@ -118,28 +120,28 @@ function update_chart(chart_id, title, data) {
     } catch(ignore) {
 	// IE8 doesn't allow to modify the page title
     }
-    
+
     $.each(measures, function(i, item) {
         value = item["value"];
         date = new Date(item["time"]*1000);
 	entry = [date, item["value"]];
-	
+
         data_chart.push(entry);
-	
+
       	if (!max_date || max_date < date)
-            max_date = date;        
+            max_date = date;
 	if (!min_date || min_date > date)
 	    min_date = date;
-        
+
         if (!min || value < min)
             min = value;
 	if (!max || value > max)
-            max = value;	
+            max = value;
     });
-    
-    style = { 
+
+    style = {
 	title: title,
-	axes: { 
+	axes: {
 	    xaxis: {
 		renderer: $.jqplot.DateAxisRenderer,
 		tickOptions: {
@@ -153,9 +155,9 @@ function update_chart(chart_id, title, data) {
 		max: max+1
 	    }
 	},
-	series: [ { 
-	    lineWidth: 1, 
-	    showMarker: false 
+	series: [ {
+	    lineWidth: 1,
+	    showMarker: false
 	} ]
     };
 
@@ -173,7 +175,7 @@ function update_menu() {
 	url = "details.html?id="+escape("/api/1.1/cpu/usage");
 	link = "<a href='"+url+"'>usage</a>";
 	str += "<li>"+link+"</li>";
-	str += "</li></ul>";	
+	str += "</li></ul>";
 
 	str += "<li><a href=\"index.html#network\">Network</a></li>";
 
@@ -187,7 +189,7 @@ function update_menu() {
 	    str += "<li>"+link+"</li>";
 	});
 	str += "</li></ul>";
-	
+
 	$("#menu-list").append(str);
 
     });
@@ -200,7 +202,7 @@ function update_summary_sensors() {
     $.getJSON("/api/1.1/sensors", function(data) {
 	$("#sensors tbody").html("");
 
-        $.each(data, function(i, item) {	    
+        $.each(data, function(i, item) {
             name = item["name"];
             type = item["type"];
             value_str = value_to_str(item["last_measure"]["value"], type);
@@ -215,8 +217,8 @@ function update_summary_sensors() {
 				 +"<td>"+min_str+"</td>"
 				 +"<td>"+max_str+"</td>"
 				 +"<td>"+type_str+"</td>"
-				 +"</tr>");                 
-        });          
+				 +"</tr>");
+        });
     });
 }
 
@@ -237,37 +239,37 @@ function update_summary_sysinfo() {
         var uptime_mn = Math.floor((uptime / 60) % 60);
         var uptime_h = Math.floor((uptime / (60*60)) % 24);
         var uptime_d = Math.floor(uptime / (60*60*24));
-	
+
         $("#cpu").append("<tr><td><a href='details.html?id=/api/1.1/cpu/usage'>"+load+"%</a></td><td>"
 			 +load_1+"</td><td>"
 			 +load_5+"</td><td>"
 			 +load_15+"</td></tr>");
-	
+
         $("#uptime").append(uptime_d+"d "+uptime_h+"h "+uptime_mn+"mn");
-	
+
         var ram = data["ram"];
         var swap = data["swap"];
         var mu = data["mem_unit"];
-	
+
 	var ramtotal = ram["total"]*mu;
         var ramfree = ram["free"]*mu;
         var ramused = (ram["total"] - ram["free"])*mu;
         var ramshared = ram["shared"]*mu;
         var rambuffer = ram["buffer"]*mu;
-        
-	
+
+
         $("#memory").append("<td>Memory</td>"
 			    +"<td>"+format_mem_size(ramtotal)+"</td>"
 			    +"<td>"+format_mem_size(ramused)+"</td>"
 			    +"<td>"+format_mem_size(ramfree)+"</td>"
 			    +"<td>"+format_mem_size(ramshared)+"</td>"
 			    +"<td>"+format_mem_size(rambuffer)+"</td>");
-	
+
         $("#swap").append("<td>Swap</td>"
 			  +"<td>"+format_mem_size(swap["total"]*mu)+"</td>"
 			  +"<td>"+format_mem_size(swap["total"]*mu-swap["free"]*mu)+"</td>"
 			  +"<td>"+format_mem_size(swap["free"]*mu)+"</td>");
-	
+
         var netdata = data["net"];
         $.each(netdata, function(i, item) {
             $("#net").append("<tr><td>"+item["name"]+"</td>"
